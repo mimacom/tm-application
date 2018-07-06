6e5b45e2852c2524b6b43c3491aae4d751a4ed5b
Ivan Greguric Ortolan <ivan.g.ortolan@gmail.com>
2018-07-06 17:51:14 +0200

    added font awesome with fonticon pipe

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,14 +3,14 @@ import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 import {Apollo, ApolloModule} from 'apollo-angular';
 import {HttpLink, HttpLinkModule} from 'apollo-angular-link-http';
 import {InMemoryCache} from 'apollo-cache-inmemory';
-import {split} from 'apollo-link';
-import {WebSocketLink} from 'apollo-link-ws';
-import {getMainDefinition} from 'apollo-utilities';
 import {NativeScriptFormsModule} from 'nativescript-angular/forms';
 import {NativeScriptHttpClientModule} from 'nativescript-angular/http-client';
 import {NativeScriptModule} from 'nativescript-angular/nativescript.module';
+import 'nativescript-localstorage';
 import {NativeScriptUISideDrawerModule} from 'nativescript-ui-sidedrawer/angular';
+const NativeWebSocket = require('nativescript-websockets');
 
+import {TNSFontIconModule} from 'nativescript-ngx-fonticon';
 import {AppComponent} from '~/app.component';
 import {AppRoutingModule} from '~/app.routing';
 import {AuthGuard} from '~/guards/auth.guard';
@@ -20,10 +20,6 @@ import {AuthService} from '~/services/auth.service';
 import {AuthInterceptor} from '~/util/auth.interceptor';
 import {JwtInterceptor} from '~/util/jwt.interceptor';
 
-import {OperationDefinitionNode} from 'graphql';
-import 'nativescript-localstorage';
-
-const NativeWebSocket = require('nativescript-websockets');
 const CONFIG = require('./config/config.json');
 
 @NgModule({
@@ -34,10 +30,13 @@ const CONFIG = require('./config/config.json');
         NativeScriptModule,
         NativeScriptFormsModule,
         NativeScriptHttpClientModule,
-        AppRoutingModule,
         ApolloModule,
         HttpLinkModule,
-        NativeScriptUISideDrawerModule
+        TNSFontIconModule.forRoot({
+            fa: './fonts/font-awesome.css'
+        }),
+        NativeScriptUISideDrawerModule,
+        AppRoutingModule
     ],
     declarations: [
         AppComponent,
@@ -72,24 +71,24 @@ export class AppModule {
         });
 
         /*
-        const ws = new WebSocketLink({
-            uri: CONFIG.apiWsEndpoint,
-            options: {
-                reconnect: true
-            },
-            webSocketImpl: NativeWebSocket
-        });
+         const ws = new WebSocketLink({
+         uri: CONFIG.apiWsEndpoint,
+         options: {
+         reconnect: true
+         },
+         webSocketImpl: NativeWebSocket
+         });
 
-        const link = split(
-            // split based on operation type
-            ({query}) => {
-                const {kind, operation} = getMainDefinition(query) as OperationDefinitionNode;
-                return kind === 'OperationDefinition' && operation === 'subscription';
-            },
-            ws,
-            http
-        );
-        */
+         const link = split(
+         // split based on operation type
+         ({query}) => {
+         const {kind, operation} = getMainDefinition(query) as OperationDefinitionNode;
+         return kind === 'OperationDefinition' && operation === 'subscription';
+         },
+         ws,
+         http
+         );
+         */
 
         apollo.create({
             link,
